24dbd2dba8fb64f581bce7fbddced93e3e00ff5b
Karnik Kanojia <karnikk1406120@gmail.com>
2024-11-05 15:14:12 +0530

    Feat: Image caching for the inference

--- components/ImagePreview.tsx
+++ components/ImagePreview.tsx
@@ -1,25 +1,21 @@
-/* eslint-disable @next/next/no-img-element */
 import { useEffect, useRef } from "react";
+import Image from "next/image";
 
 const ImagePreview: React.FC<{ url: string }> = ({ url }) => {
-  const imageRef = useRef<HTMLImageElement>(null);
+  const imageURL = useRef<string>(`/api/image?fileName=${url}`);
 
   useEffect(() => {
     if (!url) {
       console.debug("No image URL provided.");
       return;
     }
-    
-    if (imageRef.current) {
-      imageRef.current.src = `/api/image?fileName=${url}`;
-    }
+    imageURL.current = "/api/image?fileName=" + url;
   }, [url]);
 
   return url ? (
     <div className="relative h-[400px] w-[400px]">
-      <img
-        ref={imageRef}
-        src={`/api/image?fileName=${url}`} // Initial src
+      <Image
+        src={imageURL.current} // Initial src
         onError={(e) => { e.currentTarget.src = "/chest_placeholder.jpeg"; }}
         alt="Chest Xray images probably overlapped with heatmaps for identification."
         width={400}
